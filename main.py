97fab6c2b141e45df353379fd9887a06fbc62ea0
Naira Rahman <nairar01@gmail.com>
2026-07-19 19:52:53 +0400

    Initial weather alert code

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+import smtplib
+import os
+
+api_key = "6a929261f28cee0e0ed2f40a7e8ff49c"
+
+url = "https://api.openweathermap.org/data/2.5/forecast"
+
+my_email = "nairar01@gmail.com"
+password = "evdpkmantofpplvm"
+
+parameters = {
+    "lat": 54.978252,
+    "lon": -1.617780,
+    "appid": api_key,
+    "cnt" : 4
+}
+
+response = requests.get(url=url, params=parameters)
+response.raise_for_status()
+data = response.json()
+
+will_rain = False
+
+for hour_data in data['list']:
+    weather = hour_data['weather'][0]['id']
+    if weather < 700:
+        will_rain = True
+
+if will_rain:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,to_addrs=my_email, msg ="Subject: Its going to rain! \n\nBring an umbrella!")
